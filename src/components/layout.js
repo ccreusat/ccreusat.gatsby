69e9c3282cc8d1af1940fb3111cccd263609dac7
Clément Creusat <clement.creusat@gmail.com>
2021-06-27 21:30:14 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import smoothscroll from "smoothscroll-polyfill"
+// import smoothscroll from "smoothscroll-polyfill"
 
 import Header from "./header"
 import Footer from "./footer"
@@ -18,7 +18,7 @@ import "@fontsource/poppins/500.css"
 import "@fontsource/poppins/700.css"
 import "./layout.css"
 
-smoothscroll.polyfill()
+// smoothscroll.polyfill()
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
